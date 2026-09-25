fe838ed3c9dc5402dd4583b0cb713d0ac7ff5db1
Harsh M <88232654+godhunter98@users.noreply.github.com>
2026-09-25 15:00:07 +0530

    [383] [Time Beats: 40.72%] [Memory Beats: 35.46%] - LeetPush

--- 383-Ransom-Note.py
+++ 383-Ransom-Note.py
@@ -1,22 +1,16 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomcount = {}
         mag_count = {}
-        for i in ransomNote:
-            if i in ransomcount:    
-             ransomcount[i] +=1
-            else:
-                ransomcount[i] = 1
         for i in magazine:
             if i in mag_count:    
              mag_count[i] +=1
             else:
                 mag_count[i] = 1
-        result = True
-        print(ransomcount)
-        print(mag_count)
-        for char, count in ransomcount.items():
-            if mag_count.get(char,0) < count:
-                result = False
-        return result
-                
+        for c in ransomNote:
+            if c not in mag_count:
+                return False
+            if mag_count[c] == 1:
+                del mag_count[c]
+            else:
+                mag_count[c]-=1 
+        return True
