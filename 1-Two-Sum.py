20433dba36e06b41099043957e12f93a1e9192b2
Harsh M <88232654+godhunter98@users.noreply.github.com>
2026-09-25 21:42:56 +0530

    [1] [Time Beats: 53.44%] [Memory Beats: 42.54%] - LeetPush

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -1,7 +1,8 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        rev = nums[::-1]
-        for i,val in enumerate(nums):
-            for j,dec in enumerate(rev):
-                if val + dec == target and i != len(nums)-j-1:
-                    return [i,len(nums)-j-1]        
+        visited = {}
+        for indice, val in enumerate(nums):
+            num = target - val
+            if num in visited:
+                return [visited[num], indice]
+            visited[val] = indice
